f45b7df49f32fcb9c3aaa985d91dc86465bed301
Victor Yuri <victoryuri.af@gmail.com>
2022-02-10 10:50:03 -0300

    using miragejs 'api' and implementing TransactionsModal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,16 @@ import logoImg from '../../assets/logo.svg';
 
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal } : HeaderProps) {
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dtMoney" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     New Transaction
                 </button>
             </Content>
